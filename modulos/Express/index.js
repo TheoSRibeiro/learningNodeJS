eb24061886fa65bdabefcf117a8e5b95967e1029
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-08 17:22:22 -0300

    criando parametros para as rotas

--- modulos/Express/index.js
+++ modulos/Express/index.js
@@ -15,6 +15,14 @@ app.get("/blog", function(req, res){
     res.send("Bem vindo ao meu blog!")
 })
 
+//criando parametros para as rotas
+app.get('/ola/:nome/:cargo/:cor', function(req, res){
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + 
+            "<h2>Seu cargo eh: " + req.params.cargo + "</h2>" + 
+            "<h3>Sua cor favorita eh: " + req.params.cor + "</h3>") //so pode enviar o send uma unica vez na rota
+})
+
+
 //abrir servidor e deixar rodando
 app.listen(8081, function(){
 
