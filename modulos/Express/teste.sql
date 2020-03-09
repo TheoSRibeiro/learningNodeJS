be3beea0aba6bb94a47d5c833c65d986328c6cf8
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-09 16:13:03 -0300

    Manipulando o MySQL - Inserção e Consulta

--- modulos/Express/teste.sql
+++ modulos/Express/teste.sql
@@ -1,5 +1,42 @@
+--acesso ao mysql pelo terminal: mysql -h localhost -u root -p
+
 CREATE TABLE usuarios(
     nome VARCHAR(50),
     email VARCHAR(100),
     idade INT
+);
+
+INSERT INTO usuarios(nome,
+                    email,
+                    idade) 
+        VALUES( "Theo Ribeiro", 
+                "theo@gmail.com", 
+                28
+        );
+
+INSERT INTO usuarios
+    (nome,
+    email,
+    idade)
+VALUES( "Lorrany Cabral",
+        "lorryc@gmail.com",
+        24
+);
+
+INSERT INTO usuarios
+    (nome,
+    email,
+    idade)
+VALUES( "Maria",
+        "maria@gmail.com",
+        52
+);
+
+INSERT INTO usuarios
+    (nome,
+    email,
+    idade)
+VALUES( "Joao",
+        "joao@gmail.com",
+        28
 );
